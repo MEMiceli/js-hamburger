a1ac9204bb10ab5c49c850f9761f433ac4c36888
MEMiceli <marcello.emilio.miceli@gmail.com>
2022-04-29 15:49:56 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+// Hamburger menu:
+// mostrare / nascondere il menu principale
+// (ricordate che per vedere l’hamburger in alto a destra dovete scendere a risoluzioni tipo tablet o mobile).
+// Ricordate di scrivere solo il codice JavaScript e non modificare HTML e CSS
+// Consiglio del giorno:
+// Partite dal comprendere bene come è fatto il DOM e il CSS, potete usarli a vostro vantaggio!
